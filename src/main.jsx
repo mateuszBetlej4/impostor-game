9147d5203c0af7062b530201b0c9b931f3447ec5
mateuszBetlej4 <52321089+mateuszBetlej4@users.noreply.github.com>
2026-06-11 21:55:36 +0200

    Add hidden word audit route

--- src/main.jsx
+++ src/main.jsx
@@ -1,13 +1,16 @@
 import React from 'react';
 import { createRoot } from 'react-dom/client';
 import App from './App.jsx';
+import { WordAuditPage } from './dev/WordAuditPage.jsx';
 import './styles.css';
 import './passOrder.css';
 import './modes.css';
 import './logoTweaks.css';
 
+const isWordAuditRoute = window.location.hash === '#breadcrumb-words';
+
 createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    {isWordAuditRoute ? <WordAuditPage /> : <App />}
   </React.StrictMode>,
 );

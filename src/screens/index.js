0ec3ae60844c78dc3f4cc07be2a57357b93cf309
mateuszBetlej4 <52321089+mateuszBetlej4@users.noreply.github.com>
2026-06-13 17:54:25 +0200

    Update screen exports

--- src/screens/index.js
+++ src/screens/index.js
@@ -2,5 +2,6 @@ export * from './BonusVoteScreen.jsx';
 export * from './ClueScreen.jsx';
 export * from './ConfirmScreen.jsx';
 export * from './GuessScreen.jsx';
+export * from './ResultScreen.jsx';
 export * from './RevealScreen.jsx';
 export * from './VoteScreen.jsx';
